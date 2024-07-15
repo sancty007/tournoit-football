900baecfb045002b0ae24d5cd6f47bde66593c2f
Shinny22 <134633616+Shinny22@users.noreply.github.com>
2024-07-15 13:32:19 +0100

    Amelioration de la liste  de l'affichage des liste des equipes,arbitre et administrateur

--- frontend/src/components/Admin/Content/Equipe.tsx
+++ frontend/src/components/Admin/Content/Equipe.tsx
@@ -5,11 +5,12 @@ import admin_image from "/images/admin_image.png";
 import ballon from "/images/ballon.png";
 import arbitre from "/images/arbitre.png";
 import { Card } from "../../ui/card";
+import { Button } from "../../ui/button";
 
 export const Equipe = () => {
     // Modal toggle
     const [showModal, setShowModal] = useState(false);
-    const [modalType, setModalType] = useState("");
+    const [modalType, setModalType] = useState<"equipes" | "arbitres" | "administrateurs">("equipes");
     const [entries, setEntries] = useState({
         equipes: [],
         arbitres: [],
@@ -21,36 +22,64 @@ export const Equipe = () => {
     const [showArbitreList, setShowArbitreList] = useState(false);
     const [showAdminList, setShowAdminList] = useState(false);
 
-    const handleOpenModal = (type: SetStateAction<string>) => {
+    // State to handle editing
+    const [editIndex, setEditIndex] = useState<number | null>(null);
+    const [editValue, setEditValue] = useState("");
+
+    const handleOpenModal = (type: "equipes" | "arbitres" | "administrateurs") => {
         setModalType(type);
         setShowModal(true);
     };
-    
-    type modalType = "equipes" | "arbitres" | "administrateurs";
-    
+
     const handleCloseModal = () => {
         setShowModal(false);
+        setEditIndex(null);
+        setEditValue("");
     };
 
     const handleSave = (value: string) => {
         if (value !== "") {
-            setEntries((prev) => ({
-                ...prev,
-                [modalType]: [...prev[modalType], value]
-            }));
-            setShowModal(false);
-
-            // Toggle the display state for the relevant type
-            if (modalType === "equipes") {
-                setShowEquipeList(true);
-            } else if (modalType === "arbitres") {
-                setShowArbitreList(true);
-            } else if (modalType === "administrateurs") {
-                setShowAdminList(true);
+            if (editIndex !== null) {
+                setEntries((prev) => ({
+                    ...prev,
+                    [modalType]: prev[modalType].map((item, index) =>
+                        index === editIndex ? value : item
+                    )
+                }));
+            } else {
+                setEntries((prev) => ({
+                    ...prev,
+                    [modalType]: [...prev[modalType], value]
+                }));
+                // Toggle the display state for the relevant type
+                if (modalType === "equipes") {
+                    setShowEquipeList(true);
+                } else if (modalType === "arbitres") {
+                    setShowArbitreList(true);
+                } else if (modalType === "administrateurs") {
+                    setShowAdminList(true);
+                }
             }
+            setShowModal(false);
+            setEditIndex(null);
+            setEditValue("");
         } else {
             alert("Veuillez réessayer, le champ est vide");
         }
+    };
+
+    const handleEdit = (index: number, value: string, type: "equipes" | "arbitres" | "administrateurs") => {
+        setEditIndex(index);
+        setEditValue(value);
+        setModalType(type);
+        setShowModal(true);
+    };
+
+    const handleDelete = (index: number, type: "equipes" | "arbitres" | "administrateurs") => {
+        setEntries((prev) => ({
+            ...prev,
+            [type]: prev[type].filter((_, i) => i !== index)
+        }));
     };
 
     const [toggle, setToggle] = useState(1);
@@ -102,7 +131,7 @@ export const Equipe = () => {
             <div className={toggle === 1 ? "block" : "hidden"}>
                 <div className="flex flex-col md:flex-row items-center justify-center ">
                     {!showEquipeList && (
-                        <div className="">
+                        <div className="mt-6">
                             <Controller
                                 titre="Ajouter une équipe"
                                 content="Contenu de la boîte modale"
@@ -116,18 +145,40 @@ export const Equipe = () => {
                         isOpen={showModal}
                         onClose={handleCloseModal}
                         onSave={handleSave}
-                        titre="Ajouter une équipe"
+                        titre={editIndex !== null ? "Modifier une équipe" : "Ajouter une équipe"}
                         content="Contenu de la boîte modale"
                         placeholder="Nom de l'équipe"
+                        defaultValue={editValue}
                     />
                     {showEquipeList && (
-                        <div className="w-full mt-4">
+                        <div className="w-full mt-6">
                             <h3 className="text-lg font-semibold mb-2">Liste des équipes</h3>
-                            <ul>
+                            <ul className="list-disc pl-5">
                                 {entries.equipes.map((equipe, index) => (
-                                    <li className="border-2 border-black pl-2" key={index}>{equipe}</li>
+                                    <li className="border-b-2 border-gray-300 py-2 flex justify-between items-center" key={index}>
+                                        {equipe}
+                                        <div className="flex gap-2">
+                                            <Button 
+                                                className="bg-gray-500 text-white px-2 py-1 rounded"
+                                                onClick={() => handleEdit(index, equipe, "equipes")}
+                                            >Modifier
+                                            </Button>
+                                            <Button 
+                                                className="bg-red-500 text-white px-2 py-1 rounded"
+                                                onClick={() => handleDelete(index, "equipes")}
+                                            >
+                                                Supprimer
+                                            </Button>
+                                        </div>
+                                    </li>
                                 ))}
                             </ul>
+                            <Button 
+                                className="mt-4  px-4 py-2 rounded"
+                                onClick={() => handleOpenModal("equipes")}
+                            >
+                                Ajouter une équipe
+                            </Button>
                         </div>
                     )}
                 </div>
@@ -149,18 +200,41 @@ export const Equipe = () => {
                         isOpen={showModal}
                         onClose={handleCloseModal}
                         onSave={handleSave}
-                        titre="Ajouter un arbitre"
+                        titre={editIndex !== null ? "Modifier un arbitre" : "Ajouter un arbitre"}
                         content="Contenu de la boîte modale"
                         placeholder="Nom de l'arbitre"
+                        defaultValue={editValue}
                     />
                     {showArbitreList && (
-                        <div className="w-full mt-4">
+                        <div className="w-full mt-6">
                             <h3 className="text-lg font-semibold mb-2">Liste des arbitres</h3>
-                            <ul>
+                            <ul className="list-disc pl-5">
                                 {entries.arbitres.map((arbitre, index) => (
-                                    <li key={index}>{arbitre}</li>
+                                    <li className="border-b-2 border-gray-300 py-2 flex justify-between items-center" key={index}>
+                                        {arbitre}
+                                        <div className="flex gap-2">
+                                            <Button 
+                                                className="bg-gray-500 text-white px-2 py-1 rounded"
+                                                onClick={() => handleEdit(index, arbitre, "arbitres")}
+                                            >
+                                                Modifier
+                                            </Button>
+                                            <Button 
+                                                className="bg-red-500 text-white px-2 py-1 rounded"
+                                                onClick={() => handleDelete(index, "arbitres")}
+                                            >
+                                                Supprimer
+                                            </Button>
+                                        </div>
+                                    </li>
                                 ))}
                             </ul>
+                            <Button 
+                                className="mt-4  px-4 py-2 rounded"
+                                onClick={() => handleOpenModal("arbitres")}
+                            >
+                                Ajouter un arbitre
+                            </Button>
                         </div>
                     )}
                 </div>
@@ -182,18 +256,41 @@ export const Equipe = () => {
                         isOpen={showModal}
                         onClose={handleCloseModal}
                         onSave={handleSave}
-                        titre="Ajouter un administrateur"
+                        titre={editIndex !== null ? "Modifier un administrateur" : "Ajouter un administrateur"}
                         content="Contenu de la boîte modale"
                         placeholder="Nom de l'administrateur"
+                        defaultValue={editValue}
                     />
                     {showAdminList && (
-                        <div className="w-full mt-4">
+                        <div className="w-full mt-6">
                             <h3 className="text-lg font-semibold mb-2">Liste des administrateurs</h3>
-                            <ul>
+                            <ul className="list-disc pl-5">
                                 {entries.administrateurs.map((administrateur, index) => (
-                                    <li key={index}>{administrateur}</li>
+                                    <li className="border-b-2 border-gray-300 py-2 flex justify-between items-center" key={index}>
+                                        {administrateur}
+                                        <div className="flex gap-2">
+                                            <Button 
+                                                className="bg-gray-500 text-white px-2 py-1 rounded"
+                                                onClick={() => handleEdit(index, administrateur, "administrateurs")}
+                                            >
+                                                Modifier
+                                            </Button>
+                                            <Button 
+                                                className="bg-red-500 text-white px-2 py-1 rounded"
+                                                onClick={() => handleDelete(index, "administrateurs")}
+                                            >
+                                                Supprimer
+                                            </Button>
+                                        </div>
+                                    </li>
                                 ))}
                             </ul>
+                            <Button 
+                                className="mt-4  px-4 py-2 rounded"
+                                onClick={() => handleOpenModal("administrateurs")}
+                            >
+                                Ajouter un administrateur
+                            </Button>
                         </div>
                     )}
                 </div>
